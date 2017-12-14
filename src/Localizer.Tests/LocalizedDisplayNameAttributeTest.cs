e3ade170958ae137e8ea3c9ad9f2f214892ddf9d
Alden <eng.ngmalden@gmail.com>
2017-12-14 23:06:46 +0800

    Create Unit Test for LocalizedDisplayNameAttribute

---
+++ src/Localizer.Tests/LocalizedDisplayNameAttributeTest.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreLocalizer.Abstraction;
+using AspNetCoreLocalizer.Attributes;
+using Moq;
+using Xunit;
+
+namespace AspNetCoreLocalizer.Tests
+{
+    public class LocalizedDisplayNameAttributeTest
+    {
+        #region Fields
+
+        private Mock<ILocalizerService> _mockLocalizerService;
+
+        private LocalizedDisplayNameAttribute _localizedDisplayNameAttribute;
+
+        #endregion
+
+        [Fact(DisplayName = "LocalizedDisplayNameAttribute: Set the localized value of the provided resource key as DisplayName")]
+        public void LocalizedDisplayName_WithExistingResourceKey_ReturnsTheLocalizedValue()
+        {
+            //Arrange
+            //Arrange
+            _mockLocalizerService = new Mock<ILocalizerService>();
+
+            _mockLocalizerService.Setup(m => m.GetLocalizedValue("Email")).Returns("Email Address");
+
+            this._localizedDisplayNameAttribute = new LocalizedDisplayNameAttribute(_mockLocalizerService.Object, "Email");
+
+            //Act
+            var result = _localizedDisplayNameAttribute.DisplayName;
+
+            //Assert
+            Assert.Equal("Email Address", result);
+        }
+    }
+}
--- src/Localizer/Attributes/LocalizedDisplayNameAttribute.cs
+++ src/Localizer/Attributes/LocalizedDisplayNameAttribute.cs
@@ -1,23 +1,41 @@
 ﻿using System.ComponentModel;
+using AspNetCoreLocalizer.Abstraction;
+using AspNetCoreLocalizer.Services;
 
 namespace AspNetCoreLocalizer.Attributes
 {
     public  class LocalizedDisplayNameAttribute : DisplayNameAttribute
     {
+        #region Fields
+
         private readonly string _resourceKey;
+
+        private ILocalizerService _localizerService;
+
+        #endregion
 
         #region C'tor
 
-        public LocalizedDisplayNameAttribute(string resourceKey)
+        private LocalizedDisplayNameAttribute(ILocalizerService localizerService)
+        {
+            this._localizerService = localizerService;
+        }
+
+        internal LocalizedDisplayNameAttribute(ILocalizerService localizerService, string resourceKey) : this(localizerService)
         {
             this._resourceKey = resourceKey;
         }
 
+        public LocalizedDisplayNameAttribute(string resourceKey) : this(new LocalizerService(Configuration.LocalizationProvider))
+        {
+            this._resourceKey = resourceKey;
+        }
+       
         #endregion
 
         #region Public Properties
 
-        public override string DisplayName =>  Localizer.Instance[_resourceKey];
+        public override string DisplayName => _localizerService.GetLocalizedValue(_resourceKey);
 
         #endregion
     }
